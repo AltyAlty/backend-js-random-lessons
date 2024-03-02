14275c36159973a57ae9d9e2b3e515250dedd738
Alty <heneveraskedforthis@gmail.com>
2024-03-02 13:14:08 +0300

    lesson 029 005-mongodb-tutorial-001 add more info

--- 003-my-third-backend/src/db/db.ts
+++ 003-my-third-backend/src/db/db.ts
@@ -12,7 +12,23 @@ db.getCollection('books').insertMany(
         {id: 1, title: 'book-one', customersCount: 5},
         {id: 2, title: 'book-two', customersCount: 6},
         {id: 3, title: 'book-three', customersCount: 7},
-        {id: 4, title: 'book-four', customersCount: 8}
+        {id: 4, title: 'book-four', customersCount: 8},
+        {id: 5, title: 'book-five', customersCount: 4},
+        {id: 6, title: 'book-six', customersCount: 2},
+        {id: 7, title: 'book-seven', customersCount: 18},
+        {id: 8, title: 'book-eight', customersCount: 1},
+        {id: 9, title: 'book-nine', customersCount: 28},
+        {id: 10, title: 'book-ten', customersCount: 5},
+        {id: 11, title: 'book-eleven', customersCount: 3},
+        {id: 12, title: 'book-twelve', customersCount: 4},
+        {id: 13, title: 'book-thirteen', customersCount: 6},
+        {id: 14, title: 'book-fourteen', customersCount: 13},
+        {id: 15, title: 'book-fifteen', customersCount: 16},
+        {id: 16, title: 'book-sixteen', customersCount: 21},
+        {id: 17, title: 'book-seventeen', customersCount: 8},
+        {id: 18, title: 'book-eighteen', customersCount: 2},
+        {id: 19, title: 'book-nineteen', customersCount: 4},
+        {id: 20, title: 'book-twenty', customersCount: 22}
     ]
 )
 
