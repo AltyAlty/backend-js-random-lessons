9a84051fcb47ae29a4324814700af3e6d83713f6
Alty <heneveraskedforthis@gmail.com>
2025-07-25 19:56:02 +0300

    lesson 006 002-my-second-backend update comments

--- 002-my-second-backend/server2.js
+++ 002-my-second-backend/server2.js
@@ -1,24 +1,40 @@
+/*
+Установка: установить Node.js
+
+Запуск приложения: node server2.js
+
+Это HTTP-сервер, который прослушивает протокол HTTP на порту 3003 (lesson 006).
+
+Проверка приложения:
+Перейти на http://localhost:3003/
+Перейти на http://localhost:3003/home
+*/
+
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    switch (request.url) {
-        case '/home' : {
-            /*Здесь мы используем метод "setTimeout()" для имитации асинхронной работы. То есть теперь если мы сделаем
-            3 запроса на сервер, то каждый из них выполнится через 3 секунды.*/
-            setTimeout(() => {
-                const data = 'HOME';
-                response.write(data);
+const server = http.createServer(
+    (request, response) => {
+        switch (request.url) {
+            case '/home' : {
+                /*Используем метод "setTimeout()" для имитации асинхронной работы. Если сделать 3 запроса на сервер, то
+                каждый из них обработается через 3 секунды.*/
+                setTimeout(
+                    () => {
+                        const data = 'HOME';
+                        response.write(data);
+                        response.end();
+                    }, 3000
+                );
+
+                break;
+            }
+
+            default: {
+                response.write('404');
                 response.end();
-            }, 3000);
-
-            break;
-        }
-
-        default: {
-            response.write('404');
-            response.end();
+            }
         }
     }
-});
+);
 
 server.listen(3003);
--- 002-my-second-backend/server3.js
+++ 002-my-second-backend/server3.js
@@ -1,25 +1,42 @@
+/*
+Установка: установить Node.js
+
+Запуск приложения: node server3.js
+
+Это HTTP-сервер, который прослушивает протокол HTTP на порту 3003 (lesson 006).
+
+Проверка приложения:
+Перейти на http://localhost:3003/
+Перейти на http://localhost:3003/home
+*/
+
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    switch (request.url) {
-        case '/home' : {
-            setTimeout(() => {
-                const data = 'HOME';
-                response.write(data);
-            }, 3000);
-            break;
+const server = http.createServer(
+    (request, response) => {
+        switch (request.url) {
+            case '/home' : {
+                setTimeout(
+                    () => {
+                        const data = 'HOME';
+                        response.write(data);
+                    }, 3000
+                );
+
+                break;
+            }
+
+            default: {
+                response.write('404');
+            }
         }
 
-        default: {
-            response.write('404');
-        }
+        /*В данном случае метод "end()" вызывается только в одном месте уже после работы switch/case. Поскольку метод
+        "setTimeout()" порождает макрозадачу, которая не останавливает общий поток выполнения кода, то получается так,
+        что этот метод "end()" срабатывает раньше, чем срабатывает callback-функция в методе "setTimeout()", то есть
+        ответ будет закрыт раньше, чем будет видна работа callback-функции в методе "setTimeout()".*/
+        response.end();
     }
-
-    /*В данном случае метод "end()" мы вызываем только в одном месте уже после switch/case. Из-за этого поскольку
-    метод "setTimeout()" работает асинхронно, то есть не останавливает общий поток выполнения кода, то получается так,
-    что этот метод "end()" сработает раньше, чем сработает callback-функция в методе "setTimeout()", то есть мы ничего
-    не увидим.*/
-    response.end();
-});
+);
 
 server.listen(3003);
